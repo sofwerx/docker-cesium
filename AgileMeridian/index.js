952ed49a7effc9da91061e6927aefbddec72dc59
Tracey Birch <tracey.birch@sofwerx.org>
2018-07-20 08:47:34 -0400

    traffic light

--- AgileMeridian/index.js
+++ AgileMeridian/index.js
@@ -1,44 +1,20 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NGJiNTUwMy0wMDU5LTQ0MzMtODM2NS0xNjM4ZDZmMGJkYjgiLCJpZCI6MjE5MywiaWF0IjoxNTMxOTQ4NTM0fQ.74ujs8JSOZnbRJbLhf2RtrrFQyTZ5tQ7yA0xA1xqTw0';
 
-    var datafile = '/AgileMeridian/edges2.json';
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    shouldAnimate : false,
+    timeline : true
+});
 
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NGJiNTUwMy0wMDU5LTQ0MzMtODM2NS0xNjM4ZDZmMGJkYjgiLCJpZCI6MjE5MywiaWF0IjoxNTMxOTQ4NTM0fQ.74ujs8JSOZnbRJbLhf2RtrrFQyTZ5tQ7yA0xA1xqTw0';
+var datafile = '/AgileMeridian/edges2.json';
+var scene = viewer.scene;
+var statusDisplay = document.getElementById('toolbar');
+//var statusDisplay = document.createElement('div');
 
-    var viewer = new Cesium.Viewer('cesiumContainer');
-
-    var scene = viewer.scene;
-
-    var center = Cesium.Cartesian3.fromDegrees(-82.4374762, 27.9561611, 0.0)
-    var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center)
-    var heading = Cesium.Math.toRadians(90.0);
-    var pitch = Cesium.Math.toRadians(45.0);
-    var range = 500.0;
-
-//    var orangeCylinder = viewer.entities.add({
-//        name : 'Warning',
-//        position: Cesium.Cartesian3.fromDegrees(-82.4374762, 27.9561611, 0.0),
-//        cylinder : {
-//            length : 100.0,
-//            topRadius : 200.0,
-//            bottomRadius : 100.0,
-//            material : Cesium.Color.ORANGE.withAlpha(0.5),
-//            outline : true,
-//            outlineColor : Cesium.Color.ORANGE
-//        }
-//    });
-//    var blueWall = viewer.entities.add({
-//        name : 'Blue wall with sawtooth heights and outline',
-//        wall : {
-//            positions : Cesium.Cartesian3.fromDegreesArray([-82.4365, 27.952,
-//                                                            -82.4365, 27.95,
-//                                                            -82.4365, 27.947,
-//                                                            -82.4365, 27.944 ]),
-//            maximumHeights : [100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100],
-//            minimumHeights : [0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0],
-//            material : Cesium.Color.RED.withAlpha(0.5),
-//            outline : true,
-//            outlineColor : Cesium.Color.RED
-//        }
-//});
+var center = Cesium.Cartesian3.fromDegrees(-82.4374762, 27.9561611, 0.0)
+var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center)
+var heading = Cesium.Math.toRadians(270.0);
+var pitch = Cesium.Math.toRadians(60.0);
+var range = 500.0;
 
 var pts;
 var dot;
@@ -46,52 +22,89 @@ loadJSON(function(response) {
     pts = JSON.parse(response);
 });
 
-    for (var i = 0; i < pts.length; ++i) {
-        var c = Cesium.Color.GREEN;
-        var ol = Cesium.Color.DARKGREEN;
 
-        if (pts[i]._source.location.speed < 10.0) {
-	    c = Cesium.Color.RED;
-	    ol = Cesium.Color.DARKRED;
-        } else if (pts[i]._source.location.speed < 20.0) {
-            c = Cesium.Color.ORANGE;
-            ol = Cesium.Color.DARKORANGE;
-        }
+viewer.camera.lookAt(center, new Cesium.HeadingPitchRange(heading, pitch, range));
 
-        dot = viewer.entities.add({
-          position : Cesium.Cartesian3.fromDegrees(pts[i]._source.geo.lon, pts[i]._source.geo.lat),
-          point : {
-              pixelSize : 2,
-              color : c,
-              outlineColor : ol,
-              outlineWidth : 1
-          }
-        });
+for (var i = 0; i < pts.length; ++i) {
+    var c = Cesium.Color.GREEN;
+    var ol = Cesium.Color.DARKGREEN;
+    var locacc = pts[i]._source.location.locationAccuracy;
+
+    var dotdesc = '<img height="100px" style="float: left; margin: 1em;" src="images/greenlight.png" /><p style="font-size: 200%; font-weight: bold; color: green; padding-bottom: 25px;">High</p><p style="padding-bottom: 5px;">Lat: ' + pts[i]._source.geo.lat + '<br />Lon: ' +  pts[i]._source.geo.lon + '</p>';
+
+    if (locacc < 8.0) {
+        c = Cesium.Color.RED;
+        ol = Cesium.Color.DARKRED;
+        var dotdesc = '<img height="100px" style="float: left; margin: 1em;" src="images/redlight.png" /><p style="font-size: 200%; font-weight: bold; color: red; padding-bottom: 25px;">Low</p><p style="padding-bottom: 5px;">Lat: ' + pts[i]._source.geo.lat + '<br />Lon: ' +  pts[i]._source.geo.lon + '</p>';
+    } else if (locacc < 13.0) {
+        c = Cesium.Color.ORANGE;
+        ol = Cesium.Color.DARKORANGE;
+        var dotdesc = '<img height="100px" style="float: left; margin: 1em;" src="images/yellowlight.png" /><p style="font-size: 200%; font-weight: bold; color: orange; padding-bottom: 25px;">Moderate</p><p style="padding-bottom: 5px;">Lat: ' + pts[i]._source.geo.lat + '<br />Lon: ' +  pts[i]._source.geo.lon + '</p>';
     }
 
+//    ti = new Cesium.TimeIntervalCollection();
+//
+//    d = new Date(pts[i]._source.location.timestamp);
+//    ti.addInterval(
+//	Cesium.TimeInterval.fromIso8601(options : {
+//          iso8601 :  d.toISOString()
+//        });
+//    );
+//    ti.addInterval(
+//        Cesium.TimeInterval.fromIso8601(options : {
+//          iso8601 : Date.now().toISOString()
+//        })
+//    );
 
-var entity = viewer.entities.add({
-    position : Cesium.Cartesian3.fromDegrees(-82.435, 27.95616,25),
-    model : {
-        uri : '../../../../Apps/SampleData/models/CesiumBalloon/CesiumBalloon.glb',
-        scale : 3.0
-    }
+    dot = viewer.entities.add({
+      position : Cesium.Cartesian3.fromDegrees(pts[i]._source.geo.lon, pts[i]._source.geo.lat),
+//      availability : ti,
+      name: 'GPS Signal Confidence',
+      description : dotdesc,
+      point : {
+          pixelSize : 2,
+          color : c,
+          outlineColor : ol,
+          outlineWidth : 1
+      },
+    });
+}
+
+
+var vehicle = viewer.entities.add({
+  position : Cesium.Cartesian3.fromDegrees(-82.435, 27.95616,25),
+  model : {
+      uri : '../../../../Apps/SampleData/models/CesiumBalloon/CesiumBalloon.glb',
+      scale : 3.0
+  }
 });
-                                    
-    viewer.terrainProvider = Cesium.createWorldTerrain();
+                                  
+// C
+//var clock = new Cesium.Clock({
+//   startTime : Cesium.JulianDate.fromIso8601("2018-06-01"),
+//   currentTime : Cesium.JulianDate.fromIso8601("2018-06-15"),
+//   stopTime : Cesium.JulianDate.fromIso8601("2018-06-30"),
+//   clockRange : Cesium.ClockRange.LOOP_STOP,
+//   clockStep : Cesium.ClockStep.SYSTEM_CLOCK_MULTIPLIER,
+//   shouldAnimate : true
+//});
+//viewer.ClockViewModel = new Cesium.ClockViewModel(clock);
 
-    viewer.camera.lookAt(center, new Cesium.HeadingPitchRange(heading, pitch, range));     
 
-function loadJSON(callback) {   
+///////////////////////////////////////////////
 
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', datafile, false); // Replace with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
+function loadJSON(callback) {
+
+  var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+  xobj.open('GET', datafile, false); // Replace with the path to your file
+  xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+          callback(xobj.responseText);
+        }
+  };
+  xobj.send(null);
  }
+
+
